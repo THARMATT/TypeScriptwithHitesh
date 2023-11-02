e5720cab8c1ba6fa58b7c349d64ecc25cc4362b8
THARMATT <nigamsharma@gmail.com>
2023-11-02 17:16:54 +0530

    interfaces

--- 02_Basics/Myinterfaces.ts
+++ 02_Basics/Myinterfaces.ts
@@ -1,1 +1,21 @@
-interface
+interface User{
+    name:string,
+    email:string,
+    id:number,
+    startTrail:()=>string,
+    startTrail2():string,
+    getCoupon(couponName:string):number
+}
+
+//loose form of class like type
+const nigM:User={email:'h@gmail.com',id:55,name:'nigam',
+startTrail:()=>{
+    return 'logic'
+},startTrail2:()=>{
+    return 'logic'
+},
+getCoupon:(name:'Nigam')=>{
+    return 10
+}}
+
+//little bit more about interface
